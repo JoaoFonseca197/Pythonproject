8061e3ae9c85977e90a7fa0bbba09dacc2b9e200
João Fonseca <joao.hc.fonseca197@gmail.com>
2019-12-26 23:11:39 +0000

    menu

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame.freetype
 def main():
     
     pygame.init()
-    
+    click = 0
     res = (1000, 640)
     
     screen = pygame.display.set_mode(res)
@@ -13,21 +13,25 @@ def main():
     while(True):
         for event in pygame.event.get():
             c_x,c_y = pygame.mouse.get_pos()
+            mb = pygame.mouse.get_pressed()
             if(event.type == pygame.QUIT):
                 exit()
         pos = pygame.mouse.get_pos()
-        r, g, b1, b2 = 255 , 255, 0, 0
+        r, g, b1, = 255 , 255, 0, 
         screen.fill((0,0,20))
         pygame.draw.rect(screen, (r,g,b1), (450,270,100,30), 4)
         pygame.draw.rect(screen, (r,g,b1), (450,310,100,30), 4)
         pygame.draw.rect(screen, (r,g,b1), (450,350,100,30), 4)
-        pygame.draw.rect(screen, (r,g,b2), (450,390,100,30), 4)
-        pygame.draw.rect(screen, (r,g,b2), (450,430,100,30), 4)
-        pygame.draw.rect(screen, (r,g,b2), (450,470,100,30), 4)
+        pygame.draw.rect(screen, (r,g,b1), (450,390,100,30), 4)
+        pygame.draw.rect(screen, (r,g,b1), (450,430,100,30), 4)
+        pygame.draw.rect(screen, (r,g,b1), (450,470,100,30), 4)
         if(c_x >= 450 and c_x <= 550 ):
             if (c_y >= 270 and c_y <= 300):
                 b1 = 200
                 pygame.draw.rect(screen, (r,g,b1), (450,270,100,30), 4)
+                if(mb[0]):
+                    click += 1
+                    print(click)
             elif (c_y >= 310 and c_y <= 340):
                 b1 = 200
                 pygame.draw.rect(screen, (r,g,b1), (450,310,100,30), 4)
