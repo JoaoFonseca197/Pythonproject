9cd5cf40014fa5a89541d1738f39485ee59f8a69
João Fonseca <joao.hc.fonseca197@gmail.com>
2019-12-29 16:51:16 +0000

    feito classe das cartas

--- level.py
+++ level.py
@@ -1,14 +1,25 @@
 import pygame
+import random
+
 
 
 
 def main():
-    verde = (0,255,0)
-    azul = (0,0,255)
-    lilas =(177,33,199)
-    vermelho = (255,0,0)
-    cores = [verde,azul,lilas,vermelho]
-    pygame.init()
+    class Cartas:
+        def __init__(self,r,g,b):
+            self.color = (r,g,b)
+
+    C_azul = Cartas(0,0,255)
+    C_verde = Cartas(0,255,0)
+    C_lilas = Cartas(177,33,199)
+    C_vermelho = Cartas(255,0,0)
+
+   
+    cartas = [C_azul.color,C_azul.color,C_lilas.color,C_lilas.color,C_verde.color,C_verde.color,C_vermelho.color,C_vermelho.color]
+    random.shuffle(cartas)
+
+    
+    
     
     res = (1000, 640)
     
@@ -19,21 +30,32 @@ def main():
             
             if(event.type == pygame.QUIT):
                 exit()
+            
 
         r, g, b1, = 255 , 255, 0, 
         screen.fill((0,0,20))
-        pygame.draw.rect(screen, cores[0], (465,100,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (465,210,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (465,320,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (465,430,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (365,100,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (365,210,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (365,320,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (365,430,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (565,100,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (565,210,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (565,320,70,100), 4)
-        pygame.draw.rect(screen, (r,g,b1), (565,430,70,100), 4)
+        pygame.draw.rect(screen, (r,g,b1), (465,100,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (465,210,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (465,320,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (465,430,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (365,100,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (365,210,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (365,320,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (365,430,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (565,100,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (565,210,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (565,320,70,100), 0)
+        pygame.draw.rect(screen, (r,g,b1), (565,430,70,100), 0)
+        
+        pygame.draw.rect(screen,cartas[0], (465,100,70,100), 4)
+        pygame.draw.rect(screen,cartas[1], (465,210,70,100), 4)
+        pygame.draw.rect(screen,cartas[2], (465,320,70,100), 4)
+        pygame.draw.rect(screen,cartas[3], (465,430,70,100), 4)
+        pygame.draw.rect(screen,cartas[4], (365,100,70,100), 4)
+        pygame.draw.rect(screen,cartas[5], (365,210,70,100), 4)
+        pygame.draw.rect(screen,cartas[6], (365,320,70,100), 4)
+        pygame.draw.rect(screen,cartas[7], (365,430,70,100), 4)
+
         pygame.display.flip()
 
 main()
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def main():
     pygame.init()
     click = 0
     res = (1000, 640)
-    
+    my_font = pygame.freetype.Font(Noto_Sans.ttf, 24)
     screen = pygame.display.set_mode(res)
-
+    yellow = (255,255,0)
     while(True):
         for event in pygame.event.get():
             c_x,c_y = pygame.mouse.get_pos()
@@ -25,6 +25,7 @@ def main():
         pygame.draw.rect(screen, (r,g,b1), (450,390,100,30), 4)
         pygame.draw.rect(screen, (r,g,b1), (450,430,100,30), 4)
         pygame.draw.rect(screen, (r,g,b1), (450,470,100,30), 4)
+        my_font.render_to(screen, (450, 270), "3x4", yellow)
         if(c_x >= 450 and c_x <= 550 ):
             if (c_y >= 270 and c_y <= 300):
                 b1 = 200
