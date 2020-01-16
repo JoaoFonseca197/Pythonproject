7b2db2d0e06dcc3dfa139910b11e7db416a3c89c
João Fonseca <joao.hc.fonseca197@gmail.com>
2020-01-16 08:35:38 +0000

    mudanças de casa

--- level.py
+++ level.py
@@ -5,6 +5,8 @@ import random
 
 
 def main():
+    #criar criamos as cartas
+    """criamos simplesmente a cor. com isto é só preciso fazer shufle de cores para as posiçoes defenidas"""
     class Cartas:
         def __init__(self,r,g,b):
             self.color = (r,g,b)
@@ -14,7 +16,7 @@ def main():
     C_lilas = Cartas(177,33,199)
     C_vermelho = Cartas(255,0,0)
 
-   
+    #fazer shuffle
     cartas = [C_azul.color,C_azul.color,C_lilas.color,C_lilas.color,C_verde.color,C_verde.color,C_vermelho.color,C_vermelho.color]
     random.shuffle(cartas)
 
@@ -30,31 +32,57 @@ def main():
             
             if(event.type == pygame.QUIT):
                 exit()
-            
+
+        pos = pygame.mouse.get_pos()    
 
         r, g, b1, = 255 , 255, 0, 
         screen.fill((0,0,20))
-        pygame.draw.rect(screen, (r,g,b1), (465,100,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (465,210,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (465,320,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (465,430,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (365,100,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (365,210,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (365,320,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (365,430,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (565,100,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (565,210,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (565,320,70,100), 0)
-        pygame.draw.rect(screen, (r,g,b1), (565,430,70,100), 0)
-        
-        pygame.draw.rect(screen,cartas[0], (465,100,70,100), 4)
-        pygame.draw.rect(screen,cartas[1], (465,210,70,100), 4)
-        pygame.draw.rect(screen,cartas[2], (465,320,70,100), 4)
-        pygame.draw.rect(screen,cartas[3], (465,430,70,100), 4)
-        pygame.draw.rect(screen,cartas[4], (365,100,70,100), 4)
-        pygame.draw.rect(screen,cartas[5], (365,210,70,100), 4)
-        pygame.draw.rect(screen,cartas[6], (365,320,70,100), 4)
-        pygame.draw.rect(screen,cartas[7], (365,430,70,100), 4)
+        #Quadrados com as cores trocadas
+        pygame.draw.rect(screen,cartas[0], (465,100,70,100), 0)
+        pygame.draw.rect(screen,cartas[1], (465,210,70,100), 0)
+        pygame.draw.rect(screen,cartas[2], (465,320,70,100), 0)
+        pygame.draw.rect(screen,cartas[3], (465,430,70,100), 0)
+        pygame.draw.rect(screen,cartas[4], (365,100,70,100), 0)
+        pygame.draw.rect(screen,cartas[5], (365,210,70,100), 0)
+        pygame.draw.rect(screen,cartas[6], (365,320,70,100), 0)
+        pygame.draw.rect(screen,cartas[7], (365,430,70,100), 0)
+        #Quadrados amarelos
+        Q1 = pygame.draw.rect(screen, (r,g,b1), (465,100,70,100), 0)
+        Q2 = pygame.draw.rect(screen, (r,g,b1), (465,210,70,100), 0)
+        Q3 = pygame.draw.rect(screen, (r,g,b1), (465,320,70,100), 0)
+        Q4 = pygame.draw.rect(screen, (r,g,b1), (465,430,70,100), 0)
+        Q5 = pygame.draw.rect(screen, (r,g,b1), (365,100,70,100), 0)
+        Q6 = pygame.draw.rect(screen, (r,g,b1), (365,210,70,100), 0)
+        Q7 = pygame.draw.rect(screen, (r,g,b1), (365,320,70,100), 0)
+        Q8 = pygame.draw.rect(screen, (r,g,b1), (365,430,70,100), 0)
+        Q9 = pygame.draw.rect(screen, (r,g,b1), (565,100,70,100), 0)
+        Q10 = pygame.draw.rect(screen, (r,g,b1), (565,210,70,100), 0)
+        Q11 = pygame.draw.rect(screen, (r,g,b1), (565,320,70,100), 0)
+        Q12 = pygame.draw.rect(screen, (r,g,b1), (565,430,70,100), 0)
+        #Quadrados com as cores trocadas
+         if(pos[0] >= 365 and pos[0] <= 365+70 or pos[0] >= 465 and pos[0] <= 465+70 or pos[0] >= 565 and pos[0] <= 565+70 ):
+            if (pos[1] >= 270 and pos[1] <= 300):
+                b1 = 200
+                pygame.draw.rect(screen, (r,g,b1), (450,270,100,30), 4)
+                if(mb[0]):
+                    click += 1
+                    print(click)
+            elif (pos[1] >= 310 and pos[1] <= 340):
+                b1 = 200
+                pygame.draw.rect(screen, (r,g,b1), (450,310,100,30), 4)
+            elif (pos[1] >= 350 and pos[1] <= 380):
+                b1 = 200
+                pygame.draw.rect(screen, (r,g,b1), (450,350,100,30), 4)
+            elif (pos[1] >= 390 and pos[1] <= 420):
+                b1 = 200
+                pygame.draw.rect(screen, (r,g,b1), (450,390,100,30), 4)
+            elif (pos[1] >= 430 and pos[1] <= 460):
+                b1 = 200
+                pygame.draw.rect(screen, (r,g,b1), (450,430,100,30), 4)
+            elif (pos[1] >= 470 and pos[1] <= 500):
+                b1 = 200
+                pygame.draw.rect(screen, (r,g,b1), (450,470,100,30), 4) 
+
 
         pygame.display.flip()
 
--- main.py
+++ main.py
@@ -7,12 +7,11 @@ def main():
     pygame.init()
     click = 0
     res = (1000, 640)
-    my_font = pygame.freetype.Font(Noto_Sans.ttf, 24)
+    
     screen = pygame.display.set_mode(res)
     yellow = (255,255,0)
     while(True):
         for event in pygame.event.get():
-            c_x,c_y = pygame.mouse.get_pos()
             mb = pygame.mouse.get_pressed()
             if(event.type == pygame.QUIT):
                 exit()
@@ -25,26 +24,26 @@ def main():
         pygame.draw.rect(screen, (r,g,b1), (450,390,100,30), 4)
         pygame.draw.rect(screen, (r,g,b1), (450,430,100,30), 4)
         pygame.draw.rect(screen, (r,g,b1), (450,470,100,30), 4)
-        if(c_x >= 450 and c_x <= 550 ):
-            if (c_y >= 270 and c_y <= 300):
+        if(pos[0] >= 450 and pos[0] <= 550 ):
+            if (pos[1] >= 270 and pos[1] <= 300):
                 b1 = 200
                 pygame.draw.rect(screen, (r,g,b1), (450,270,100,30), 4)
                 if(mb[0]):
                     click += 1
                     print(click)
-            elif (c_y >= 310 and c_y <= 340):
+            elif (pos[1] >= 310 and pos[1] <= 340):
                 b1 = 200
                 pygame.draw.rect(screen, (r,g,b1), (450,310,100,30), 4)
-            elif (c_y >= 350 and c_y <= 380):
+            elif (pos[1] >= 350 and pos[1] <= 380):
                 b1 = 200
                 pygame.draw.rect(screen, (r,g,b1), (450,350,100,30), 4)
-            elif (c_y >= 390 and c_y <= 420):
+            elif (pos[1] >= 390 and pos[1] <= 420):
                 b1 = 200
                 pygame.draw.rect(screen, (r,g,b1), (450,390,100,30), 4)
-            elif (c_y >= 430 and c_y <= 460):
+            elif (pos[1] >= 430 and pos[1] <= 460):
                 b1 = 200
                 pygame.draw.rect(screen, (r,g,b1), (450,430,100,30), 4)
-            elif (c_y >= 470 and c_y <= 500):
+            elif (pos[1] >= 470 and pos[1] <= 500):
                 b1 = 200
                 pygame.draw.rect(screen, (r,g,b1), (450,470,100,30), 4) 
         
